90da2e6cb6e4c29f84abc04704304bc3acaadd96
MTD-SONNT290\Dell <nguyenthaison0201@gmail.com>
2023-03-20 21:08:17 +0700

    add task-deleting and time-checker

--- src/utils/checkDeadline.js
+++ src/utils/checkDeadline.js
@@ -1,6 +1,24 @@
-
+import { useEffect } from "react";
+import { ActionEnum, useTasks } from "../stores";
 const TIME = 60*1000;
 
-export const checkDeadline = (tasks) => {
-    
-}
+export const CheckDeadline = () => {
+  const [tasks, dispatchTasks] = useTasks();
+  useEffect(() => {
+    const checker = setInterval(() => {
+      const now = new Date();
+      tasks.forEach((task) => {
+        //  console.log(now);
+        const deadline = new Date(task.deadline).getTime();
+        // console.log(deadline <= now);
+        if (!task.isDone && deadline <= now) {
+          console.log(`${task.name} hết giờ`);
+          dispatchTasks({ type: ActionEnum.TOGGLE_TASK, payload: task.id });
+        }
+      });
+    }, TIME);
+
+    return () => clearInterval(checker);
+  }, [tasks]);
+  return null;
+};
